103d6efda50239e01a4205b506b49b9405ea811d
gsayle <sayleman56@gmail.com>
2026-09-19 14:41:13 -0400

    Deleted scripts not needed. Added script to download the data, added LLM usage.

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,37 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data on deaths of people experiencing
+# homelessness in Toronto from Open Data Toronto.
+# Author: Graham Sayle
+# Date: 19 September 2026
+# Contact: g.sayle@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites:
+# - `opendatatoronto` must be installed (install.packages("opendatatoronto"))
+# - `tidyverse` must be installed (install.packages("tidyverse"))
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# Get the package (dataset) metadata
+package <- search_packages("Deaths of People Experiencing Homelessness")
+
+# List every resource (file) attached to the package
+resources <- list_package_resources("8546b6d9-41ec-48df-895f-09e86784ef26")
+
+# Keep the resource that breaks deaths down by cause
+cause_resource <-
+  resources |>
+  filter(name == "Homeless deaths by cause")
+
+# Download it
+raw_data <- get_resource(cause_resource)
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+dir.create("data/01-raw_data", recursive = TRUE, showWarnings = FALSE)
+write_csv(raw_data, "data/01-raw_data/.csv")
